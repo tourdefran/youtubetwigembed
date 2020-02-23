ea1298df607d0e735328026565ce5238d085f853
tourdefran <francoarias@gmail.com>
2020-02-23 14:55:37 -0300

    fixing

--- src/youtubeTwigEmbedExtension.php
+++ src/youtubeTwigEmbedExtension.php
@@ -11,19 +11,15 @@ use Bolt\Extension\SimpleExtension;
  */
 class youtubeTwigEmbedExtension extends SimpleExtension
 {
-	public function isSafe()
-	{
-	    return true;
-	}	
 
-    public function initialize()
-    {
-        $this->addTwigFunction('youtube', 'twigYoutube');
-    	$this->addTwigFunction('vimeo', 'twigVimeo');
-    }
+    protected function registerTwigFunctions() {
+        return [
+            'youtube' => 'twigYoutube',
+            'vimeo' => 'twigVimeo'
+        ];
+    }    
 
-	function twigYoutube($id = "")
-	{
+	public function twigYoutube($id = "") {
 
         $html = '<div class="artist-video"><div class="flex-video"><iframe width="560" height="315" src="https://www.youtube.com/embed/%id%" frameborder="0" allowfullscreen></iframe></div></div>';
         
@@ -32,8 +28,7 @@ class youtubeTwigEmbedExtension extends SimpleExtension
         return new \Twig_Markup($html, 'UTF-8');        
 	}
 
-    function twigVimeo($id = "")
-    {
+    public function twigVimeo($id = "") {
 
         $html = '<div class="artist-video"><div class="flex-video"><iframe src="https://player.vimeo.com/video/%id%?title=0&byline=0&portrait=0&badge=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></div>';
         
@@ -41,9 +36,5 @@ class youtubeTwigEmbedExtension extends SimpleExtension
 
         return new \Twig_Markup($html, 'UTF-8');        
     }
-
-    public function getName()
-    {
-        return "youtube";
-    }	
+    	
 }
